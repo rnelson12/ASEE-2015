8f9570a471615d6bcaa05ab64b109fe7408d17c6
wsen18 <wsennett3@gmail.com>
2015-02-11 16:44:57 -0600

    Made test sketch for conveyor

--- Code/Libraries/Conveyor/examples/Test_Sketch/Test_Sketch.ino
+++ Code/Libraries/Conveyor/examples/Test_Sketch/Test_Sketch.ino
@@ -1,12 +1,73 @@
 #include <Servo.h>
 #include <Conveyor.h>
-
+Conveyor *conveyor;
+int motorSpeed = 100;
+int closedAngle =  0; //actually open angle
+int openAngle = 133;//actually closed angle
+byte conveyorMotorForwardPin = 33;
+byte conveyorMotorBackwardPin = 34;
+byte clawMotorPin = 9;
 void setup() 
 {
-  
+ Serial.begin(9600); 
+ conveyor = new Conveyor(motorSpeed,closedAngle,openAngle,conveyorMotorForwardPin,conveyorMotorBackwardPin,clawMotorPin);
 }
 
 void loop() 
 {
-
+  while(true)
+  {
+   (*conveyor).openClaw();
+   delay(3000);
+   (*conveyor).closeClaw();
+   delay(1000);
+  }
+  Serial.println("Go to fish to position");
+ (*conveyor).goToPosition(FISH);
+ delay(250);
+ (*conveyor).openClaw();
+ delay(250);
+ (*conveyor).closeClaw();
+ delay(250);
+ 
+  Serial.println("Go to fish to red bin");
+ (*conveyor).goToPosition(RED_BIN);
+ delay(250);
+ (*conveyor).openClaw();
+ delay(250);
+ (*conveyor).closeClaw();
+ delay(250);
+ 
+ Serial.println("Go to fish to yellow bin");
+ (*conveyor).goToPosition(YELLOW_BIN);
+ delay(250);
+ (*conveyor).openClaw();
+ delay(250);
+ (*conveyor).closeClaw();
+ delay(250);
+ 
+ Serial.println("Go to fish to blue bin");
+ (*conveyor).goToPosition(BLUE_BIN);
+ delay(250);
+ (*conveyor).openClaw();
+ delay(250);
+ (*conveyor).closeClaw();
+ delay(250);
+ 
+ Serial.println("Go to fish to green bin");
+ (*conveyor).goToPosition(GREEN_BIN);
+ delay(250);
+ (*conveyor).openClaw();
+ delay(250);
+ (*conveyor).closeClaw();
+ delay(250);
+ 
+ Serial.println("Return to fish position");
+ (*conveyor).goToPosition(FISH);
+ delay(250);
+ (*conveyor).openClaw();
+ delay(250);
+ (*conveyor).closeClaw();
+ delay(250);
+ 
 }
